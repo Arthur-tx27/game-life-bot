6abc563b86be5ad8810e4567656552bf85b74081
Arthur <arturka_tx@mail.ru>
2026-05-21 15:32:25 +0300

    fix lint config

--- eslint.config.js
+++ eslint.config.js
@@ -13,6 +13,6 @@ export default tseslint.config(
     },
   },
   {
-    ignores: ['dist/', 'node_modules/', 'prisma/'],
+    ignores: ['dist/', 'node_modules/', 'prisma/', 'src/generated/'],
   },
 );
